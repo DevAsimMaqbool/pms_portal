1e57bfbfd6cb19f2f5abdb9336811d3f0acbda41
Ahmad-Raiwind <raiwind.pkistan@gmail.com>
2025-10-30 11:38:10 +0500

    v2 add md

--- resources/views/admin/v2.blade.php
+++ resources/views/admin/v2.blade.php
@@ -592,7 +592,7 @@
 
         
 
-       <div class="card mb-6" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-custom-class="tooltip-warning" data-bs-original-title="(Lecturer) Faisalabad - Uni Campus">
+       <div class="card" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-custom-class="tooltip-warning" data-bs-original-title="(Lecturer) Faisalabad - Uni Campus">
           <div class="card-body d-flex">
             <div class="d-flex w-70 align-items-center me-4">
               <div class="badge bg-label-warning rounded p-1_5 me-4"><i class="icon-base ti tabler-award icon-md"></i>
