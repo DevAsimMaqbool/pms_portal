8a5b8cd54ffda69e81e9380fa6ef63b5933c73d6
DevAsimMaqbool <asimmaqbool195@gmail.com>
2025-11-07 10:03:53 +0500

    fixes

--- resources/views/admin/award.blade.php
+++ resources/views/admin/award.blade.php
@@ -18,7 +18,8 @@
                     <div class="card-body">
                         <h5 class="card-title">Sitara-e-Qiyadat</h5>
                         <p class="card-text">Chairman’s Leadership Excellence Award</p>
-                        <a href="javascript:void(0)" class="btn btn-dark waves-effect waves-light">Explore</a>
+                        <a href="javascript:void(0)" class="btn btn-dark waves-effect waves-light" data-bs-toggle="modal"
+                            data-bs-target="#SitaraeQiyadat">Explore</a>
                     </div>
                 </div>
             </div>
@@ -82,6 +83,62 @@
 
         </div>
         <!-- Examples -->
+    </div>
+
+    <div class="modal fade" id="SitaraeQiyadat" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-xl" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel4">Sitara-e-Qiyadat</h5>
+
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col mb-4">
+                            <label for="nameEx" class="form-label">
+                                <strong>The Sitara-e-Qiyadat – Chairman’s Leadership Excellence
+                                    Award</strong>
+                                has been established to honor outstanding leaders within the Superior
+                                University community
+                                whose vision, commitment, and influence have set new benchmarks of
+                                excellence.
+                                <br>
+                                This prestigious award recognizes individuals who not only demonstrate
+                                exceptional leadership
+                                within their areas of responsibility but also embody the spirit of
+                                innovation, courage,
+                                and service that leadership demands in shaping society.
+                                <br>
+                                The Sitara-e-Qiyadat award symbolizes the pioneering spirit of Superior
+                                University:
+                                a spirit that believes in leading from the front, creating new
+                                possibilities,
+                                and making meaningful contributions to society.
+                                <br>
+                                It is a tribute to those leaders who exemplify Superior’s commitment to
+                                excellence
+                                and whose efforts ripple far beyond the boundaries.
+                            </label>
+                        </div>
+                    </div>
+                    <div class="row g-4">
+                        <div class="col mb-0">
+                            <label for="emailExLarge" class="form-label">Email</label>
+                            <input type="email" id="emailExLarge" class="form-control" placeholder="xxxx@xxx.xx" />
+                        </div>
+                        <div class="col mb-0">
+                            <label for="dobExLarge" class="form-label">DOB</label>
+                            <input type="date" id="dobExLarge" class="form-control" />
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-label-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
