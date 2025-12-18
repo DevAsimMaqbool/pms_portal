e2f263f63717f7ffbaa49ee9c54ab7225c52f12d
Ahmad-Raiwind <raiwind.pkistan@gmail.com>
2025-12-18 15:31:45 +0500

    Stop tracking view cache files

--- storage/framework/views/1e0e42c06126d4f72ce05a0f7978fd59.php
+++
@@ -1,2 +0,0 @@
-<img style="width: 150px;" src="<?php echo e(asset('admin/assets/img/avatars/superior.svg')); ?>">
-<?php /**PATH C:\wamp64\www\pms\resources\views/components/application-logo.blade.php ENDPATH**/ ?>
--- storage/framework/views/e849e8949e24cfbb4bf8ab6b0a88684f.php
+++
@@ -1,5 +0,0 @@
-<button <?php echo e($attributes->merge(['type' => 'submit', 'class' => 'btn btn-primary d-grid w-100'])); ?>>
-    <?php echo e($slot); ?>
-
-</button>
-<?php /**PATH C:\wamp64\www\pms\resources\views\components\primary-button.blade.php ENDPATH**/ ?>
