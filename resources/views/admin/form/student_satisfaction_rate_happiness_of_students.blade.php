c517294fce3ccc6fcf6c6eed600ec1a94b9702b2
Ahmad-Raiwind <raiwind.pkistan@gmail.com>
2026-02-02 10:45:38 +0500

    add design setting

--- resources/views/admin/form/student_satisfaction_rate_happiness_of_students.blade.php
+++ resources/views/admin/form/student_satisfaction_rate_happiness_of_students.blade.php
@@ -68,7 +68,7 @@
             <div class="card-datatable table-responsive card-body">
             <div class="d-flex justify-content-between">
                                <div>
-                                <h5 class="mb-1"></h5>
+                                <h5 class="mb-1">Student Satisfaction Rate / Happiness of students</h5>
                                 </div>
                                 <a href="{{ route('indicators_crud.index', ['slug' => 'Internationalization_section', 'id' => $indicatorId]) }}" class="btn rounded-pill btn-outline-primary waves-effect"> View</a>
                             </div> 
