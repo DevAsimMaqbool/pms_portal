71b9367d91c0e183e05843c3b9aa4b115e438871
Ahmad-Raiwind <raiwind.pkistan@gmail.com>
2026-03-27 18:28:56 +0500

    add datas

--- resources/views/admin/indicator_crud/no_of_programs_accredited_or_affiliated_nationally_internationally_and_ranking.blade.php
+++ resources/views/admin/indicator_crud/no_of_programs_accredited_or_affiliated_nationally_internationally_and_ranking.blade.php
@@ -15,7 +15,7 @@
 @section('content')
     <!-- Content -->
     <div class="container-xxl flex-grow-1 container-p-y">
-        @if(in_array(getRoleName(activeRole()), ['Dean']))
+        @if(in_array(getRoleName(activeRole()), ['Dean','HOD','Program Leader UG']))
         <!-- Multi Column with Form Separator -->
         <div class="card">
             <div class="card-header d-flex align-items-center justify-content-between">
@@ -27,7 +27,7 @@
                 </div>
             </div>
             <div class="card-datatable table-responsive card-body">
-                    @if(in_array(getRoleName(activeRole()), ['Dean']))
+                    @if(in_array(getRoleName(activeRole()), ['Dean','HOD','Program Leader UG']))
                         <div class="tab-pane fade show" id="form2" role="tabpanel">
                            <div class="table-responsive text-nowrap">
                              <table id="intellectualTable" class="table table-bordered">
@@ -328,7 +328,7 @@
     </script>
 @endpush
 @push('script')
-    @if(in_array(getRoleName(activeRole()), ['Dean']))
+    @if(in_array(getRoleName(activeRole()), ['Dean','HOD','Program Leader UG']))
         <script>
             function fetchCommercialForms() {
                 $.ajax({
